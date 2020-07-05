b4f0e9e738655dff165fb6c239297be93323ac56
Yousef <yousefaman100@gmail.com>
2020-07-05 23:02:39 +0300

    deploy

---
+++ app.js
@@ -0,0 +1,55 @@
+/*fetch('data.json')
+.then(res => res.json())
+.then((json) => {
+  var intData = json.links[1].price;
+
+  document.getElementById('demo').innerHTML = intData ;  
+
+
+
+
+
+
+
+})
+
+*/
+
+
+fetch('https://mystifying-galileo-3ae747.netlify.app/run_results.json')
+.then(res => res.json())
+.then((json) => {
+  
+
+  document.getElementById('demo').innerHTML = "total: " + json.value ;  
+
+
+
+
+
+
+
+})
+
+/*
+
+var  staticUrl = 'https://mystifying-galileo-3ae747.netlify.app/run_results.json';
+
+$.getJSON(staticUrl , function(data) {
+	console.log(data);
+	// body...
+});
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+  
